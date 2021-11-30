c4c9fc57e7dabe407d8fa01fd2bfff99a730e1b2
Derek Wight <derek.wight@quanser.com>
2021-11-30 02:24:45 +0000

    QCar WS1-3 * Spawn list updated for all makes to ensure all actors are available * Added traffic light * Fixed bug in the silhouette people where the "move to" command was   still using the left hand coordinate system instead of the right
    git-svn-id: svn://svnviz.quanser.com/viz/QuanserSim/Models/OpenWorld/Python/Common@1107 508624a6-4b46-f746-ae76-588926622adc

--- library_qlabs_roundabout_sign.py
+++ library_qlabs_roundabout_sign.py
@@ -20,7 +20,7 @@ class QLabsRoundaboutSign:
     def spawn(self, qlabs, deviceNumber, location, rotation, scale, waitForConfirmation=True):
         return qlabs.spawn(deviceNumber, self.ID_ROUNDABOUT_SIGN, location[0], location[1], location[2], rotation[0], rotation[1], rotation[2], scale[0], scale[1], scale[2], 0, waitForConfirmation)
  
-    def spawn_spawned(self, qlabs, deviceNumber, location, rotation, scale, waitForConfirmation=True):
+    def spawnDegrees(self, qlabs, deviceNumber, location, rotation, scale, waitForConfirmation=True):
         rotation[0] = rotation[0]/180*math.pi
         rotation[1] = rotation[1]/180*math.pi
         rotation[2] = rotation[2]/180*math.pi    
---
+++ library_qlabs_trafficlight_single.py
@@ -0,0 +1,54 @@
+from library_qlabs import QuanserInteractiveLabs, CommModularContainer
+from quanser.common import GenericError
+import math
+
+import struct
+        
+        
+######################### MODULAR CONTAINER CLASS #########################
+
+class QLabsTrafficLightSingle:
+
+       
+    ID_TRAFFIC_LIGHT_SINGLE = 10051
+    
+    FCN_TRAFFIC_LIGHT_SINGLE_SET_STATE = 10
+    FCN_TRAFFIC_LIGHT_SINGLE_SET_STATE_ACK = 11
+    
+    STATE_RED = 0
+    STATE_GREEN = 1
+    STATE_YELLOW = 2
+    
+    # Initialize class
+    def __init__(self):
+
+       return
+       
+    def spawn(self, qlabs, deviceNumber, location, rotation, scale, waitForConfirmation=True):
+        return qlabs.spawn(deviceNumber, self.ID_TRAFFIC_LIGHT_SINGLE, location[0], location[1], location[2], rotation[0], rotation[1], rotation[2], scale[0], scale[1], scale[2], 0, waitForConfirmation)
+ 
+    def spawnDegrees(self, qlabs, deviceNumber, location, rotation, scale, waitForConfirmation=True):
+        rotation[0] = rotation[0]/180*math.pi
+        rotation[1] = rotation[1]/180*math.pi
+        rotation[2] = rotation[2]/180*math.pi    
+    
+        return qlabs.spawn(deviceNumber, self.ID_TRAFFIC_LIGHT_SINGLE, location[0], location[1], location[2], rotation[0], rotation[1], rotation[2], scale[0], scale[1], scale[2], 0, waitForConfirmation)
+ 
+    def setState(self, qlabs, deviceNumber, state, waitForConfirmation=True):
+        c = CommModularContainer()
+        c.classID = self.ID_TRAFFIC_LIGHT_SINGLE
+        c.deviceNumber = deviceNumber
+        c.deviceFunction = self.FCN_TRAFFIC_LIGHT_SINGLE_SET_STATE
+        c.payload = bytearray(struct.pack(">B", state))
+        c.containerSize = c.BASE_CONTAINER_SIZE + len(c.payload)
+        
+        if waitForConfirmation:
+            qlabs.flushReceive()  
+        
+        if (qlabs.sendContainer(c)):
+            if waitForConfirmation:
+                c = qlabs.waitForContainer(self.ID_TRAFFIC_LIGHT_SINGLE, deviceNumber, self.FCN_TRAFFIC_LIGHT_SINGLE_SET_STATE_ACK)
+                    
+            return True
+        else:
+            return False        
