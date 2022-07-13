d6f07840aacc7e3ee72e7a91743cbe32bf5b9b67
JUPITER3\Derek <derek.wight@quanser.com>
2022-07-13 16:50:07 -0400

    - Updates to some return types - Added set_transform_degrees - Added standard ping, get_world_transform - Added image capture and set_image_capture_resolution

--- library_qlabs_free_camera.py
+++ library_qlabs_free_camera.py
@@ -1,8 +1,10 @@
-from library_qlabs import QuanserInteractiveLabs, CommModularContainer
-from quanser.common import GenericError
-import math
+from library_qlabs_common import QLabsCommon
+from library_qlabs import CommModularContainer
 
+import math
 import struct
+import cv2
+import numpy as np
         
         
 ######################### MODULAR CONTAINER CLASS #########################
@@ -11,7 +13,7 @@ class QLabsFreeCamera:
     """ This class supports the spawning and control of free movement cameras in QLabs open worlds."""
 
     ID_FREE_CAMERA = 170 
-    """ """
+    """Class ID"""
     FCN_FREE_CAMERA_POSSESS = 10
     """ """
     FCN_FREE_CAMERA_POSSESS_ACK = 11
@@ -24,7 +26,16 @@ class QLabsFreeCamera:
     """ """
     FCN_FREE_CAMERA_SET_TRANSFORM_ACK = 15
     """ """
+    FCN_FREE_CAMERA_SET_IMAGE_RESOLUTION = 90
+    """ """
+    FCN_FREE_CAMERA_SET_IMAGE_RESOLUTION_RESPONSE = 91
+    """ """
+    FCN_FREE_CAMERA_REQUEST_IMAGE = 100
+    """ """
+    FCN_FREE_CAMERA_RESPONSE_IMAGE = 101
+    """ """
 
+    _jpg_buffer = bytearray()
     
     # Initialize class
     def __init__(self):
@@ -43,10 +54,10 @@ class QLabsFreeCamera:
         :type actorNumber: uint32
         :type location: float array[3]
         :type rotation: float array[3]
-        :return: `True` if spawn was successful, `False` otherwise
-        :rtype: boolean
+        :return: 0 if successful, 1 class not available, 2 actor number not available or already in use, 3 unknown error, -1 communications error
+        :rtype: int32
         """
-        return qlabs.spawn(actorNumber, self.ID_FREE_CAMERA, location[0], location[1], location[2], rotation[0], rotation[1], rotation[2], 1, 1, 1, 0, True)
+        return QLabsCommon().spawn(qlabs, actorNumber, self.ID_FREE_CAMERA, location, rotation, [1, 1, 1], 0, True)
            
     def spawn_degrees(self, qlabs, actorNumber, location, rotation):
         """
@@ -60,13 +71,13 @@ class QLabsFreeCamera:
         :type actorNumber: uint32
         :type location: float array[3]
         :type rotation: float array[3]
-        :return: `True` if spawn was successful, `False` otherwise
-        :rtype: boolean
+        :return: 0 if successful, 1 class not available, 2 actor number not available or already in use, 3 unknown error, -1 communications error
+        :rtype: int32
         """
         
-        return qlabs.spawn(actorNumber, self.ID_FREE_CAMERA, location[0], location[1], location[2], rotation[0]/180*math.pi, rotation[1]/180*math.pi, rotation[2]/180*math.pi, 1, 1, 1, 0, True)
+        return QLabsCommon().spawn(qlabs, actorNumber, self.ID_FREE_CAMERA,  location, [rotation[0]/180*math.pi, rotation[1]/180*math.pi, rotation[2]/180*math.pi], [1, 1, 1], 0, True)
     
-    def spawn_and_parent_with_relative_transform(self, qlabs, actorNumber, location, rotation, parentClassID, parentActorNumber, parentComponent, waitForConfirmation=True):
+    def spawn_and_parent_with_relative_transform(self, qlabs, actorNumber, location, rotation, parentClassID, parentActorNumber, parentComponent):
         """
         Spawns a camera in an instance of QLabs at a specific location and rotation relative to the parent actor using radians.
 
@@ -77,8 +88,6 @@ class QLabsFreeCamera:
         :param parentClassID: Parent class ID (Can be found in the class library)
         :param parentActorNumber: The unique actorNumber identifier for the parent class in QLabs
         :param ParentComponent: `0` for the origin of the parent actor, see the parent class for additional reference frame options
-        :param waitForConfirmation: Wait for acknowledgement from QLabs to proceed, this is a blocking operation
-            defaults to True
         :type qlabs: object
         :type actorNumber: uint32
         :type location: array[3]
@@ -86,12 +95,12 @@ class QLabsFreeCamera:
         :type parentClassID: uint32
         :type parentActorNumber: uint32
         :type ParentComponent: uint32
-        :return: `True` if spawn was successful (or if waitForConfirmation set to `False`), `False` otherwise
-        :rtype: boolean
+        :return: 0 if successful, 1 class not available, 2 actor number not available or already in use, 3 cannot find the parent actor, 4 unknown error, -1 communications error
+        :rtype: int32
         """
-        return qlabs.spawn_and_parent_with_relative_transform(actorNumber, self.ID_FREE_CAMERA, location[0], location[1], location[2], rotation[0], rotation[1], rotation[2], 1, 1, 1, 0, parentClassID, parentActorNumber, parentComponent, waitForConfirmation)
+        return QLabsCommon().spawn_and_parent_with_relative_transform(qlabs, actorNumber, self.ID_FREE_CAMERA,  location, rotation, [1, 1, 1], 0, parentClassID, parentActorNumber, parentComponent, True)
    
-    def spawn_and_parent_with_relative_transform_degrees(self, qlabs, actorNumber, location, rotation, parentClassID, parentActorNumber, parentComponent, waitForConfirmation=True):
+    def spawn_and_parent_with_relative_transform_degrees(self, qlabs, actorNumber, location, rotation, parentClassID, parentActorNumber, parentComponent):
         """
         Spawns a camera in an instance of QLabs at a specific location and rotation relative to the parent actor using degrees.
         
@@ -102,8 +111,6 @@ class QLabsFreeCamera:
         :param parentClassID: Parent class ID (Can be found in the class library)
         :param parentActorNumber: The unique actorNumber identifier for the parent class in QLabs
         :param ParentComponent: `0` for the origin of the parent actor, see the parent class for additional reference frame options
-        :param waitForConfirmation: Wait for acknowledgement from QLabs to proceed, this is a blocking operation
-            defaults to True
         :type qlabs: object
         :type actorNumber: uint32
         :type location: array[3]
@@ -111,11 +118,11 @@ class QLabsFreeCamera:
         :type parentClassID: uint32
         :type parentActorNumber: uint32
         :type ParentComponent: uint32
-        :return: `True` if spawn was successful (or if waitForConfirmation set to `False`), `False` otherwise
-        :rtype: boolean
+        :return: 0 if successful, 1 class not available, 2 actor number not available or already in use, 3 cannot find the parent actor, 4 unknown error, -1 communications error
+        :rtype: int32
 
         """
-        return qlabs.spawn_and_parent_with_relative_transform(actorNumber, self.ID_FREE_CAMERA, location[0], location[1], location[2], rotation[0]/180*math.pi, rotation[1]/180*math.pi, rotation[2]/180*math.pi, 1, 1, 1, 0, parentClassID, parentActorNumber, parentComponent, waitForConfirmation)
+        return QLabsCommon().spawn_and_parent_with_relative_transform(qlabs, actorNumber, self.ID_FREE_CAMERA,location, [rotation[0]/180*math.pi, rotation[1]/180*math.pi, rotation[2]/180*math.pi], [1, 1, 1], 0, parentClassID, parentActorNumber, parentComponent, True)
    
     
     def possess(self, qlabs, actorNumber):
@@ -184,12 +191,12 @@ class QLabsFreeCamera:
         
     def set_transform(self, qlabs, actorNumber, location, rotation):
         """
-        Change the location and rotation of a spawned camera.
+        Change the location and rotation of a spawned camera in radians
         
         :param qlabs: A QuanserInteractiveLabs object
         :param actorNumber: User defined unique identifier for the class actor in QLabs
         :param location: An array of floats for x, y and z coordinates
-        :param rotation: An array of floats for the roll, pitch, yaw
+        :param rotation: An array of floats for the roll, pitch, yaw in radians
         :type qlabs: object
         :type actorNumber: uint32
         :type location: array[3]
@@ -204,6 +211,38 @@ class QLabsFreeCamera:
         c.actorFunction = self.FCN_FREE_CAMERA_SET_TRANSFORM
         c.payload = bytearray(struct.pack(">ffffff", location[0], location[1], location[2], rotation[0], rotation[1], rotation[2]))
         c.containerSize = c.BASE_CONTAINER_SIZE + len(c.payload)
+
+        qlabs.flush_receive()  
+        
+        if (qlabs.send_container(c)):
+            c = qlabs.wait_for_container(self.ID_FREE_CAMERA, actorNumber, self.FCN_FREE_CAMERA_SET_TRANSFORM_ACK)
+                                
+            return True
+        else:
+            return False     
+
+    def set_transform_degrees(self, qlabs, actorNumber, location, rotation):
+        """
+        Change the location and rotation of a spawned camera in degrees
+        
+        :param qlabs: A QuanserInteractiveLabs object
+        :param actorNumber: User defined unique identifier for the class actor in QLabs
+        :param location: An array of floats for x, y and z coordinates
+        :param rotation: An array of floats for the roll, pitch, yaw in degrees
+        :type qlabs: object
+        :type actorNumber: uint32
+        :type location: array[3]
+        :type rotation: array[3]
+        :return: `True` if spawn was successful, `False` otherwise
+        :rtype: boolean
+
+        """
+        c = CommModularContainer()
+        c.classID = self.ID_FREE_CAMERA
+        c.actorNumber = actorNumber
+        c.actorFunction = self.FCN_FREE_CAMERA_SET_TRANSFORM
+        c.payload = bytearray(struct.pack(">ffffff", location[0], location[1], location[2], rotation[0]/180*math.pi, rotation[1]/180*math.pi, rotation[2]/180*math.pi))
+        c.containerSize = c.BASE_CONTAINER_SIZE + len(c.payload)
         
         qlabs.flush_receive()  
         
@@ -212,4 +251,105 @@ class QLabsFreeCamera:
                     
             return True
         else:
-            return False     
+            return False 
+
+    def destroy(self, qlabs, actorNumber):
+        """Destroys a camera in an instance of QLabs.
+
+        :param qlabs: A QuanserInteractiveLabs object
+        :param actorNumber: User defined unique identifier for the class actor in QLabs
+        :type qlabs: QuanserInteractiveLabs object
+        :type actorNumber: uint32
+        :return: The number of actors destroyed. -1 if failed.
+        :rtype: int32
+
+        """
+        return QLabsCommon().destroy_spawned_actor(qlabs, self.ID_FREE_CAMERA, actorNumber)
+
+
+    def ping(self, qlabs, actorNumber):
+        """Checks if a camera of the corresponding actor number exists in the QLabs environment.
+
+        :param qlabs: A QuanserInteractiveLabs object
+        :param actorNumber: User defined unique identifier for the class actor in QLabs
+        :type qlabs: QuanserInteractiveLabs object
+        :type actorNumber: uint32
+        :return: `True` if spawn was successful, `False` otherwise
+        :rtype: boolean
+
+        """
+        return QLabsCommon().ping_actor(qlabs, actorNumber, self.ID_FREE_CAMERA)
+
+    def get_world_transform(self, qlabs, actorNumber):
+        """Get the location, rotation, and scale in world coordinates of the camera
+        
+        :param qlabs: A QuanserInteractiveLabs object.
+        :param actorNumber: User defined unique identifier for the class actor in QLabs
+        :type qlabs: QuanserInteractiveLabs object
+        :type actorNumber: uint32
+        :return: success, location, rotation, scale
+        :rtype: boolean, float array[3], float array[3], float array[3]
+        """    
+        return QLabsCommon().get_world_transform(qlabs, actorNumber, self.ID_FREE_CAMERA)
+
+    def set_image_capture_resolution(self, qlabs, actorNumber, width=640, height=480):
+        """Change the default width and height of image resolution for capture
+        
+        :param qlabs: A QuanserInteractiveLabs object.
+        :param actorNumber: User defined unique identifier for the class actor in QLabs
+        :param width: Must be an even number. Default 640
+        :param height: Must be an even number. Default 480
+        :type qlabs: QuanserInteractiveLabs object
+        :type actorNumber: uint32
+        :type width: uint32
+        :type height: uint32
+        :return: `True` if spawn was successful, `False` otherwise
+        :rtype: boolean
+        """   
+
+        c = CommModularContainer()
+        c.classID = self.ID_FREE_CAMERA
+        c.actorNumber = actorNumber
+        c.actorFunction = self.FCN_FREE_CAMERA_SET_IMAGE_RESOLUTION
+        c.payload = bytearray(struct.pack(">II", width, height))
+        c.containerSize = c.BASE_CONTAINER_SIZE + len(c.payload)
+        
+        qlabs.flush_receive()  
+        
+        if (qlabs.send_container(c)):
+            c = qlabs.wait_for_container(self.ID_FREE_CAMERA, actorNumber, self.FCN_FREE_CAMERA_SET_IMAGE_RESOLUTION_RESPONSE)
+            return True
+        else:
+            return False
+
+        
+
+    def get_image(self, qlabs, actorNumber):
+        """Request an image from the camera actor
+        
+        :param qlabs: A QuanserInteractiveLabs object.
+        :param actorNumber: User defined unique identifier for the class actor in QLabs
+        :type qlabs: QuanserInteractiveLabs object
+        :type actorNumber: uint32
+        :return: Success, RGB image data (default resolution is 640x480).
+        :rtype: boolean, 
+        """   
+        c = CommModularContainer()
+        c.classID = self.ID_FREE_CAMERA
+        c.actorNumber = actorNumber
+        c.actorFunction = self.FCN_FREE_CAMERA_REQUEST_IMAGE
+        c.payload = bytearray()
+        c.containerSize = c.BASE_CONTAINER_SIZE + len(c.payload)
+        
+        qlabs.flush_receive()  
+        
+        if (qlabs.send_container(c)):
+            c = qlabs.wait_for_container(self.ID_FREE_CAMERA, actorNumber, self.FCN_FREE_CAMERA_RESPONSE_IMAGE)
+            data_size, = struct.unpack(">I", c.payload[0:4])
+
+            self._jpg_buffer = cv2.imdecode(np.frombuffer(bytearray(c.payload[4:len(c.payload)]), dtype=np.uint8, count=-1, offset=0), 1)
+            
+            
+            return True, self._jpg_buffer
+        else:
+            return False, self._jpg_buffer
