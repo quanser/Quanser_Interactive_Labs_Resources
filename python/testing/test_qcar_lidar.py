48489e138c7cf200250ee9d906baa162056acd85
Derek Wight <dereklwight@gmail.com>
2022-10-20 11:36:23 -0400

    Updated lidar test script to use the new python format

--- python/testing/test_qcar_lidar.py
+++ python/testing/test_qcar_lidar.py
@@ -12,12 +12,10 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
 
 from library_qlabs import QuanserInteractiveLabs
-from library_qlabs_common import QLabsCommon
 from library_qlabs_qcar import QLabsQCar
 from library_qlabs_basic_shape import QLabsBasicShape
 from library_qlabs_free_camera import QLabsFreeCamera
 
-from library_qlabs_utilities import *
 
 setup_only = False
 
@@ -32,18 +30,22 @@ def testLIDAR():
         print("Unable to connect to QLabs")
         return    
     
-    QLabsCommon().destroy_all_spawned_actors(qlabs)
+    qlabs.destroy_all_spawned_actors()
 
-    QLabsFreeCamera().spawn(qlabs,0,[53.696, 18.906, 51.107], [0, 0.865, -0.01])
-    QLabsFreeCamera().possess(qlabs, 0)
+    hCamera = QLabsFreeCamera(qlabs)
+    hCamera.spawn([53.696, 18.906, 51.107], [0, 0.865, -0.01])
+    hCamera.possess()
 
     
-    QLabsQCar().spawn_degrees(qlabs, actorNumber=0, location=[82.545, 28.056, 0], rotation=[0,0,0], waitForConfirmation=True)
-    #QLabsQCar().possess(qlabs, 0, QLabsQCar().CAMERA_OVERHEAD)
-    QLabsBasicShape().spawn_box_walls_from_end_points(qlabs, 0, [70, 40, 0], [130, 40, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)
-    QLabsBasicShape().spawn_box_walls_from_end_points(qlabs, 1, [130, 40, 0], [130, 0, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)
-    QLabsBasicShape().spawn_box_walls_from_end_points(qlabs, 2, [70, 0, 0], [130, 0, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)    
-    QLabsBasicShape().spawn_box_walls_from_end_points(qlabs, 3, [70, 40, 0], [70, 0, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)
+    hQCar = QLabsQCar(qlabs)
+    hQCar.spawn_degrees(location=[82.545, 28.056, 0], rotation=[0,0,0], waitForConfirmation=True)
+    #hQCar.possess(hQCar.CAMERA_OVERHEAD)
+
+    hShape = QLabsBasicShape(qlabs)
+    hShape.spawn_id_box_walls_from_end_points(0, [70, 40, 0], [130, 40, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)
+    hShape.spawn_id_box_walls_from_end_points(1, [130, 40, 0], [130, 0, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)
+    hShape.spawn_id_box_walls_from_end_points(2, [70, 0, 0], [130, 0, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)    
+    hShape.spawn_id_box_walls_from_end_points(3, [70, 40, 0], [70, 0, 0], height=3, thickness=1, colour=[1,0,0], waitForConfirmation=True)
     
     
     if (setup_only):
@@ -59,21 +61,21 @@ def testLIDAR():
     
     time.sleep(1)
     
-    QLabsQCar().set_velocity_and_request_state(qlabs, actorNumber=0, forward=5, turn=0, headlights=False, leftTurnSignal=False, rightTurnSignal=False, brakeSignal=False, reverseSignal=False)
+    hQCar.set_velocity_and_request_state(forward=5, turn=0, headlights=False, leftTurnSignal=False, rightTurnSignal=False, brakeSignal=False, reverseSignal=False)
     
     for count in range(100):
         
-        print("Reading data")
-        success, angle, distance = QLabsQCar().get_lidar(qlabs,0,samplePoints=400)
+        #print("Reading data")
+        success, angle, distance = hQCar.get_lidar(samplePoints=400)
         
         x = np.sin(angle)*distance
         y = np.cos(angle)*distance
 
         lidarData.setData(x,y)
         QtWidgets.QApplication.instance().processEvents()
-        time.sleep(0.01)
+        #time.sleep(0.001)
     
-    QLabsQCar().set_velocity_and_request_state(qlabs, actorNumber=0, forward=0, turn=0, headlights=False, leftTurnSignal=False, rightTurnSignal=False, brakeSignal=False, reverseSignal=False)
+    hQCar.set_velocity_and_request_state(forward=0, turn=0, headlights=False, leftTurnSignal=False, rightTurnSignal=False, brakeSignal=False, reverseSignal=False)
     
     
     qlabs.close()
