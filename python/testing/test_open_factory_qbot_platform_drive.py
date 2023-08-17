c6943b5aa4cb10156f4f0236b7306cd8783f3444
mrDataMeetsQ <murtaza.bohra@quanser.com>
2023-08-17 17:01:35 -0400

    Initial commit of updated QBotPlatform Workspace model and associated files LIDAR support added but missing Leishen support in Ranging Sensor

--- python/testing/test_open_factory_qbot_platform_drive.py
+++ python/testing/test_open_factory_qbot_platform_drive.py
@@ -11,7 +11,7 @@ from qvl.qbot_platform import QLabsQBotPlatform
 from qvl.qbot2e import QLabsQBot2e
 from qvl.spline_line import QLabsSplineLine
 
-#from qvl.animal import QLabsAnimal
+# from qvl.animal import QLabsAnimal
 
 import sys
 import time
@@ -20,11 +20,6 @@ import struct
 import numpy as np
 import cv2
 import keyboard
-
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtWidgets
-
-lidar_rate = 0.01
 
 
 ########### Main program #################
@@ -47,16 +42,13 @@ def main():
 
     # destroy the previous QBot and respawn it in a starting position
 
-    #qlabs.destroy_all_spawned_actors()
-    
-    pBot = QLabsQBotPlatform(qlabs)
-    pBot.actorNumber=20
-    pBot.destroy()
+    qlabs.destroy_all_spawned_actors()
 
-    hQBot = QLabsQBotPlatform(qlabs, True)
-    hQBot.spawn_id_degrees(actorNumber=20, location=[0, 0, 2], rotation=[0,0,0], scale=[1,1,1], configuration=0)
+
+    hQBot = QLabsQBotPlatform(qlabs)
+    hQBot.spawn_id_degrees(actorNumber=0, location=[0, 0, 2], rotation=[0,0,0], scale=[1,1,1], configuration=0)
     hQBot.possess(hQBot.VIEWPOINT_TRAILING,)
-    
+
     '''
     hQBot = QLabsQBot2e(qlabs)
     hQBot.spawn_id_degrees(actorNumber=0, location=[0, 0, 2], rotation=[0, 0, 0], scale=[1, 1, 1], configuration=0)
@@ -71,33 +63,25 @@ def main():
 
     hSpline.set_points([1, 0, 1], point_list, alignEndPointTangents=False, waitForConfirmation=True)
 
-
     location_x = 0
     location_y = 0
     rotation_z = 0
 
     speed = 0
 
-    
+    '''
     cv2.startWindowThread()
     cv2.namedWindow('image_stream', cv2.WINDOW_AUTOSIZE)
     image_RGBD = cv2.imread('Quanser640x480.jpg')
     cv2.imshow('image_stream', image_RGBD)
-    
+    '''
     done = False
     counter = 0
-
-
-    lidarPlot = pg.plot(title="LIDAR")
-    squareSize = 100
-    lidarPlot.setXRange(-squareSize, squareSize)
-    lidarPlot.setYRange(-squareSize, squareSize)
-    lidarData = lidarPlot.plot([], [], pen=None, symbol='o', symbolBrush='r', symbolPen=None, symbolSize=2)
 
     # -------- Main Program Loop -----------
     while not done:
 
-        
+        '''
         status, image_RGBD = hQBot.get_image(hQBot.CAMERA_RGB)
 
         if status == True:
@@ -106,15 +90,12 @@ def main():
                 image_RGBD = cv2.putText(img = image_RGBD, text = "QA, ED for tank-control driving", org = (0, 30), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255, 255, 255), thickness = 1)
             else:
                 image_RGBD = cv2.putText(img = image_RGBD, text = "WASD for steering-control driving", org = (0, 30), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255, 255, 255), thickness = 1)
-            
-            image_RGBD = cv2.putText(img = image_RGBD, text = "ESC to exit", org = (0, 45), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255, 255, 255), thickness = 1)
 
-            current_image_size = "Image size: {}x{}".format(image_RGBD.shape[1], image_RGBD.shape[0])
-            image_RGBD = cv2.putText(img = image_RGBD, text = current_image_size, org = (0, 60), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255, 255, 255), thickness = 1)
+            image_RGBD = cv2.putText(img = image_RGBD, text = "ESC to exit", org = (0, 45), fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 0.5, color = (255, 255, 255), thickness = 1)
 
             cv2.imshow('image_stream', image_RGBD)
             cv2.waitKey(1)
-        
+        '''
         if (tank_drive):
             speedScale = 1
             leftWheelSpeed = 0
@@ -164,25 +145,6 @@ def main():
         #c = hQBot.command_and_request_state(qlabs, actorNumber, rightWheelSpeed, leftWheelSpeed)
         if c == False:
             done = True
-
-
-
-        
-
-        print("Reading from LIDAR... if QLabs crashes, make sure FPS > 100 or fix the crash bug!")
-
-
-        #for count in range(20):
-
-        success, angle, distance = hQBot.get_lidar(samplePoints=400)
-        print(success)
-
-        x = np.sin(angle)*distance
-        y = np.cos(angle)*distance
-
-        lidarData.setData(x,y)
-        QtWidgets.QApplication.instance().processEvents()
-        time.sleep(lidar_rate)
 
 
     cv2.destroyAllWindows()
--- spawn_models/QBotPlatform/QBotPlatformMakeCommandStateContainer.m
+++ spawn_models/QBotPlatform/QBotPlatformMakeCommandStateContainer.m
@@ -1,12 +1,18 @@
-function Container = QBotPlatformMakeCommandStateContainer(DeviceNumber, Forward, Right) %#codegen
+function Container = QBotPlatformMakeCommandStateContainer(DeviceNumber, WheelSpeeds, LeftColor, RightColor) %#codegen
 
 %skip size for now
 
 Container = flip(typecast(int32(23), 'uint8')); %Device ID
 Container = [Container flip(typecast(int32(DeviceNumber), 'uint8'))];
 Container = [Container uint8(10)]; %Device function
-Container = [Container flip(typecast(single(Forward), 'uint8'))];
-Container = [Container flip(typecast(single(Right), 'uint8'))];
+Container = [Container flip(typecast(single(WheelSpeeds(1)), 'uint8'))];
+Container = [Container flip(typecast(single(WheelSpeeds(2)), 'uint8'))];
+Container = [Container flip(typecast(single(LeftColor(1)), 'uint8'))];
+Container = [Container flip(typecast(single(LeftColor(2)), 'uint8'))];
+Container = [Container flip(typecast(single(LeftColor(3)), 'uint8'))];
+Container = [Container flip(typecast(single(RightColor(1)), 'uint8'))];
+Container = [Container flip(typecast(single(RightColor(2)), 'uint8'))];
+Container = [Container flip(typecast(single(RightColor(3)), 'uint8'))];
 
 %Prepend container size
 Container = [flip(typecast(int32(length(Container) + 4), 'uint8')) Container]';
