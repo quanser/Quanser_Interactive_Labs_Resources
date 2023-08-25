7ea5f68c419160afc6724e23471fe3f3b239dc4b
kjfullert <100737789+kjfullert@users.noreply.github.com>
2023-08-25 10:51:07 -0400

    Deleted files before commit to public.

--- spawn_models/CoupledTanks/CoupledTanksDecodeModeContainer.m
+++
@@ -1,6 +0,0 @@
-function [Out2Connected, Tank1OutletSize, Tank2OutletSize] = CoupledTanksDecodeModeContainer(Payload, StartIndex) %#codegen
-
-Out2Connected = uint8(Payload(StartIndex));
-Tank1OutletSize = uint8(Payload(StartIndex+1));
-Tank2OutletSize = uint8(Payload(StartIndex+2));
-
